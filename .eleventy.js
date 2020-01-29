370b4584111aeba3fb92614335c9d422f35c0ea0
Peter deHaan <peter@deseloper.com>
2020-01-29 01:30:09 -0800

    Add JSON5 support

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
+const JSON5 = require("json5");
 const yaml = require("js-yaml");
 
 module.exports = eleventyConfig => {
   eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+  eleventyConfig.addDataExtension("json", contents => JSON5.parse(contents));
 
   return {};
 };
